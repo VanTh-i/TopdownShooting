91db95942560acbba6497d623bb7492148092830
VanTh-i <thaiqtdh1b@gmail.com>
2023-12-08 14:43:58 +0700

    add 16 new enemy

---
+++ Assets/Data/Scripts/Enemy/EnemyType/Giant/GiantStats.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantStats : EnemyStats
+{
+    private EnemySpawnSystem enemySpawn;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        enemySpawn = FindObjectOfType<EnemySpawnSystem>();
+    }
+    protected override void Kill()
+    {
+        base.Kill();
+        EnemySpawn.Instance.DeSpawn(transform.parent);
+        enemySpawn.OnEnemyKilled();
+        SpawnExplosion();
+    }
+    protected virtual void SpawnExplosion()
+    {
+        Transform explosion = FxSpawn.Instance.Spawn(transform.position, transform.rotation, 0);
+        explosion.gameObject.SetActive(true);
+    }
+}
