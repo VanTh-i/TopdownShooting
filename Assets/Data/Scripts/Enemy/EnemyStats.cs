052e0b3531d142575158de2186f42de739cac752
VanTh-i <thaiqtdh1b@gmail.com>
2023-12-12 00:46:48 +0700

    despawn enemy

--- Assets/Data/Scripts/Enemy/EnemyStats.cs
+++ Assets/Data/Scripts/Enemy/EnemyStats.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Random = UnityEngine.Random;
 
 public class EnemyStats : ThaiBehaviour
 {
@@ -11,6 +12,9 @@ public class EnemyStats : ThaiBehaviour
     [HideInInspector] public int currentMaxHp;
     [HideInInspector] public float currentSpeed;
     [HideInInspector] public int currentAttackRange;
+
+    protected float deSpawnDistance = 12f;
+    protected Transform player;
 
     [Header("Damage Feedback")]
     protected Color dmgColor = new Color(1, 0, 0, 1);
@@ -35,6 +39,20 @@ public class EnemyStats : ThaiBehaviour
         currentMaxHp = enemyStats.MaxHp;
         enemySprite = GetComponentInParent<SpriteRenderer>();
         originalColor = enemySprite.color;
+
+        player = FindPlayer.GetPlayer();
+        if (player == null)
+        {
+            Debug.LogError("Can not find player");
+        }
+    }
+
+    protected void Update()
+    {
+        if (Vector2.Distance(transform.parent.position, player.transform.position) >= deSpawnDistance)
+        {
+            ReturnEnemy();
+        }
     }
 
     public virtual void TakeDamage(int dmg, Vector2 sourcePosition)
@@ -71,6 +89,12 @@ public class EnemyStats : ThaiBehaviour
             return true;
         }
         else return false;
+    }
+
+    private void ReturnEnemy()
+    {
+        EnemySpawnSystem enemySpawn = FindObjectOfType<EnemySpawnSystem>();
+        transform.parent.position = player.transform.position + enemySpawn.spawnPosition[Random.Range(0, enemySpawn.spawnPosition.Count)].position;
     }
 
     protected IEnumerator DamageFlash()
